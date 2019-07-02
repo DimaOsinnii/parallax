5999d45b739e7bb05cab42977bc7bd1022ba1c14
DimaOsinnii <ozerland@gmail.com>
2019-07-02 18:04:46 +0300

    fix parallax effect

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+
 function parallax(event) {
-    let layers = this.querySelectorAll('.layer');
-    layers.forEach(layer => {
-        let speed = layer.getAttribute('data-speed');
-        layer.style.transform = `translateX(${event.clientX * speed / 1000}px)`
+ let layers = this.querySelectorAll('.layer');
+layers.forEach(layer => {
+    let speed = layer.getAttribute('data-val');
+        layer.style.transform = `translate(${event.clientX * (- 1) * speed / 1000}px, ${event.clientY  * (- 1) * speed / 1000}px)`;
     });
 }
-document.addEventListener("mousemove", parallax);
+let header = document.querySelector('header');
+header.addEventListener("mousemove", parallax);
+
+
+
+
+
+
